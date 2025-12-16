050316f7bc0048810e314110dc59027c69a142be
Kaput550 <jisaacgm7@gmail.com>
2025-12-15 22:34:26 -0600

    fix borrar notificacion

--- backend/rutas/rutasNotificaciones.js
+++ backend/rutas/rutasNotificaciones.js
@@ -11,12 +11,13 @@ const {
 const { proteger } = require('../middleware/authMiddleware');
 
 // Todas las rutas protegidas
+// IMPORTANTE: Rutas específicas ANTES de rutas con parámetros dinámicos
 router.get('/', proteger, obtenerNotificaciones);
 router.get('/no-leidas', proteger, contarNoLeidas);
-router.put('/:id/leer', proteger, marcarComoLeida);
 router.put('/leer-todas', proteger, marcarTodasLeidas);
-router.delete('/:id', proteger, eliminarNotificacion);
 router.delete('/eliminar-todas/todas', proteger, eliminarTodasNotificaciones);
+router.put('/:id/leer', proteger, marcarComoLeida);
+router.delete('/:id', proteger, eliminarNotificacion);
 
 module.exports = router;
 
